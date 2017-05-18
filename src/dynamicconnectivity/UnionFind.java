a63e2bd62c8ea0f5963e2acc827b07a6dee8f3d9
mqf20 <mingqingfoo@gmail.com>
2017-05-18 21:38:53 +0800

    Google code style changes

--- src/dynamicconnectivity/UnionFind.java
+++ src/dynamicconnectivity/UnionFind.java
@@ -1,32 +1,34 @@
 package dynamicconnectivity;
 
 /**
- * This is a parent class for Union-Find algorithms to inherit.
- * 
- * From Week 1, Coursera Algorithms, Part I (https://class.coursera.org/algs4partI-010)
+ * This is a parent class for Union-Find algorithms to inherit. From Week 1, Coursera Algorithms,
+ * Part I (https://class.coursera.org/algs4partI-010).
  * 
  * @author Ming
  *
  */
 public abstract class UnionFind {
-	
-	protected int[] id;
-	protected final int N;
-	
-	public UnionFind(int N) {
-		this.N = N;
-		id = new int[N];
-		for (int i = 0; i < N; i++) {
-			id[i] = i;
-		}
-	}
-	
-	public abstract boolean connected(int p, int q);
-	
-	public abstract void union(int p, int q);
-	
-	public int[] getId() {
-		return id;
-	}
+
+  protected int[] id;
+  protected final int numNodes;
+
+  /**
+   *  Initialize with the number of nodes.
+   */
+  public UnionFind(int n) {
+    this.numNodes = n;
+    id = new int[n];
+    for (int i = 0; i < n; i++) {
+      id[i] = i;
+    }
+  }
+
+  public abstract boolean connected(int p, int q);
+
+  public abstract void union(int p, int q);
+
+  public int[] getId() {
+    return id;
+  }
 
 }
--- src/dynamicconnectivity/WeightedQuickUnion.java
+++ src/dynamicconnectivity/WeightedQuickUnion.java
@@ -1,63 +1,57 @@
 package dynamicconnectivity;
 
 /**
- * This class implements the Weighted Quick Union algorithm explained in Week 1 that makes these 
- * improvements on the Quick Union algorithm:
+ * This class implements the Weighted Quick Union algorithm from Week 1, Coursera Algorithms, Part I
+ * (https://class.coursera.org/algs4partI-010). These improvements are made on the Quick Union
+ * algorithm:
  * 
- * - Use an "intelligent" union operation to combine the smaller tree to the larger tree, such that
- *   the "depth" of a tree is now guaranteed to be at most log N (base 2).
- *   
- * - Use path compression - while finding the root of a given node p, point every node visited to 
- * 	 its parent.
- * 
- * From Week 1, Coursera Algorithms, Part I (https://class.coursera.org/algs4partI-010)
+ * <ul>
+ * <li>Use an "intelligent" union operation to combine the smaller tree to the larger tree, such
+ * that the "depth" of a tree is now guaranteed to be at most log N (base 2).</li>
+ * <li>Use path compression - while finding the root of a given node p, point every node visited to
+ * its parent.</li>
+ * </ul>
  * 
  * @author Ming
  *
  */
 public class WeightedQuickUnion extends QuickUnion {
-	
-	private int[] size;  // Special array for counting the size (number of objects) of each tree
-	
-	public WeightedQuickUnion(int N) {
-		super(N);
-		size = new int[N];  // Elements are initialized to zero
-	}
-	
-	/**
-	 * Chase parent pointers until root is reached
-	 * 
-	 * @param node
-	 * @return root of node
-	 */
-	public int findRoot(int node) {
-		while (node != id[node]) {
-			id[node] = id[id[node]];  // point node to its grandparent (for path compression)
-			node = id[node];
-		}
-		return node;
-	}
-	
-	/**
-	 * Change root of p to root of q
-	 * 
-	 * @param p
-	 * @param q
-	 */
-	@Override
-	public void union(int p, int q) {
-		int pRoot = findRoot(p);
-		int qRoot = findRoot(q);
-		if (pRoot != qRoot) {
-			if (size[pRoot] < size[qRoot]) {
-				id[pRoot] = qRoot;
-				size[pRoot] += size[qRoot];
-			} else {
-				id[qRoot] = pRoot;
-				size[qRoot] += size[pRoot];
-			}
-			
-		}
-	}
-	
+
+  private int[] size; // Special array for counting the size (number of objects) of each tree
+
+  public WeightedQuickUnion(int n) {
+    super(n);
+    size = new int[n]; // Elements are initialized to zero
+  }
+
+  /**
+   * Chase parent pointers until root is reached.
+   */
+  public int findRoot(int node) {
+    while (node != id[node]) {
+      id[node] = id[id[node]]; // point node to its grandparent (for path compression)
+      node = id[node];
+    }
+    return node;
+  }
+
+  /**
+   * Change root of p to root of q.
+   */
+  @Override
+  public void union(int p, int q) {
+    int rootP = findRoot(p);
+    int rootQ = findRoot(q);
+    if (rootP != rootQ) {
+      if (size[rootP] < size[rootQ]) {
+        id[rootP] = rootQ;
+        size[rootP] += size[rootQ];
+      } else {
+        id[rootQ] = rootP;
+        size[rootQ] += size[rootP];
+      }
+
+    }
+  }
+
 }
