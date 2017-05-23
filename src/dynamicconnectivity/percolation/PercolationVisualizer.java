faf3f12a51252556cf4a41fbb99740a9c3eede50
mqf20 <mingqingfoo@gmail.com>
2017-05-23 15:11:35 +0800

    Modified style

--- src/dynamicconnectivity/percolation/PercolationVisualizer.java
+++ src/dynamicconnectivity/percolation/PercolationVisualizer.java
@@ -24,68 +24,72 @@ import edu.princeton.cs.algs4.StdDraw;
 
 public class PercolationVisualizer {
 
-    // delay in miliseconds (controls animation speed)
-    private static final int DELAY = 500;
+  // delay in miliseconds (controls animation speed)
+  private static final int DELAY = 500;
 
-    // draw N-by-N percolation system
-    public static void draw(Percolation perc, int N) {
-        StdDraw.clear();
-        StdDraw.setPenColor(StdDraw.BLACK);
-        StdDraw.setXscale(-.05*N, 1.05*N);
-        StdDraw.setYscale(-.05*N, 1.05*N);   // leave a border to write text
-        StdDraw.filledSquare(N/2.0, N/2.0, N/2.0);
+  /**
+  // draw N-by-N percolation system.
+   */
+  public static void draw(Percolation perc, int gridLength) {
+    StdDraw.clear();
+    StdDraw.setPenColor(StdDraw.BLACK);
+    StdDraw.setXscale(-.05 * gridLength, 1.05 * gridLength);
+    StdDraw.setYscale(-.05 * gridLength, 1.05 * gridLength); // leave a border to write text
+    StdDraw.filledSquare(gridLength / 2.0, gridLength / 2.0, gridLength / 2.0);
 
-        // draw N-by-N grid
-        int opened = 0;
-        for (int row = 1; row <= N; row++) {
-            for (int col = 1; col <= N; col++) {
-                if (perc.isFull(row, col)) {
-                    StdDraw.setPenColor(StdDraw.BOOK_LIGHT_BLUE);
-                    opened++;
+    // draw N-by-N grid
+    int opened = 0;
+    for (int row = 1; row <= gridLength; row++) {
+      for (int col = 1; col <= gridLength; col++) {
+        if (perc.isFull(row, col)) {
+          StdDraw.setPenColor(StdDraw.BOOK_LIGHT_BLUE);
+          opened++;
+        } else if (perc.isOpen(row, col)) {
+          StdDraw.setPenColor(StdDraw.WHITE);
+          opened++;
+        } else {
+          StdDraw.setPenColor(StdDraw.BLACK);
         }
-                else if (perc.isOpen(row, col)) {
-                    StdDraw.setPenColor(StdDraw.WHITE);
-                    opened++;
-                }
-                else
-                    StdDraw.setPenColor(StdDraw.BLACK);
-                StdDraw.filledSquare(col - 0.5, N - row + 0.5, 0.45);
-            }
-        }
-
-        // write status text
-        StdDraw.setFont(new Font("SansSerif", Font.PLAIN, 12));
-        StdDraw.setPenColor(StdDraw.BLACK);
-        StdDraw.text(.25*N, -N*.025, opened + " open sites");
-        if (perc.percolates()) StdDraw.text(.75*N, -N*.025, "percolates");
-        else                   StdDraw.text(.75*N, -N*.025, "does not percolate");
-
+        StdDraw.filledSquare(col - 0.5, gridLength - row + 0.5, 0.45);
+      }
     }
 
-    /**
-     * Modified by Ming for testing
-     */
-    public static void main(String[] args) {
-    	
-//        In in = new In(args[0]);      // input file
-        
-        In in = new In("src/dynamicConnectivity/percolation/input6.txt");  // Example
-        
-        int N = in.readInt();         // N-by-N percolation system
-
-        // turn on animation mode
-        StdDraw.show(0);
-
-        // repeatedly read in sites to open and draw resulting system
-        Percolation perc = new Percolation(N);
-        draw(perc, N);
-        StdDraw.show(DELAY);
-        while (!in.isEmpty()) {
-            int i = in.readInt();
-            int j = in.readInt();
-            perc.open(i, j);
-            draw(perc, N);
-            StdDraw.show(DELAY);
-        }
+    // write status text
+    StdDraw.setFont(new Font("SansSerif", Font.PLAIN, 12));
+    StdDraw.setPenColor(StdDraw.BLACK);
+    StdDraw.text(.25 * gridLength, -gridLength * .025, opened + " open sites");
+    if (perc.percolates()) {
+      StdDraw.text(.75 * gridLength, -gridLength * .025, "percolates");
+    } else {
+      StdDraw.text(.75 * gridLength, -gridLength * .025, "does not percolate");
     }
+
+  }
+
+  /**
+   * Modified for testing.
+   */
+  public static void main(String[] args) {
+
+    // In in = new In(args[0]); // input file
+
+    In in = new In("src/dynamicConnectivity/percolation/input6.txt"); // Example
+
+    int gridLength = in.readInt(); // N-by-N percolation system
+
+    // turn on animation mode
+    StdDraw.show(0);
+
+    // repeatedly read in sites to open and draw resulting system
+    Percolation perc = new Percolation(gridLength);
+    draw(perc, gridLength);
+    StdDraw.show(DELAY);
+    while (!in.isEmpty()) {
+      int i = in.readInt();
+      int j = in.readInt();
+      perc.open(i, j);
+      draw(perc, gridLength);
+      StdDraw.show(DELAY);
+    }
+  }
 }
