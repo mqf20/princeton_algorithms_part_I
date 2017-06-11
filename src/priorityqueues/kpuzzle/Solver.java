82c1585ed1836c22685f0c349f3a8ffac52e226c
mq <mingqingfoo@gmail.com>
2017-06-11 21:33:30 +0800

    First complete algorithm

--- src/priorityqueues/kpuzzle/Solver.java
+++ src/priorityqueues/kpuzzle/Solver.java
@@ -7,61 +7,86 @@ import edu.princeton.cs.algs4.StdOut;
 
 /**
  * Solver from assignment of Week 4, Coursera Algorithms, Part I
- * (https://class.coursera.org/algs4partI-010).
+ * (https://class.coursera.org/algs4partI-010) that solves a k-puzzle problem using the A* search
+ * algorithm.
  */
 public class Solver {
 
   /**
    * Constants
    */
-  public static int UNSOLVABLE = -1; // implies that this board is unsolvable
-  
+  public static final int UNSOLVABLE = -1; // implies that this board is unsolvable
+  public static final int MAX_ITERS = 1000;
+
+  public enum PriorityFunction {
+    HAMMING, MANHATTAN;
+  }
+
+  public static final PriorityFunction PRIORITY_FUNCTION_CHOICE = PriorityFunction.HAMMING;
+  // this is the priority function used by the A* search algorithm
+
   /**
    * Variables
    */
   private Stack<Board> solutionStack = new Stack<Board>();
+  private int numMoves; // number of moves required to reach the goal
   private boolean solvable = false;
 
   /**
    * Accepts an initial board and finds a solution to this board.
    */
   public Solver(Board initial) {
-    
+
     // ----- [] Primary board
-    
-    Board primaryBoard = initial;
-    MinPQ<Board> primaryPQ = new MinPQ<Board>(Board.BY_HAMMING);  // http://algs4.cs.princeton.edu/code/javadoc/edu/princeton/cs/algs4/MinPQ.html
-    primaryPQ.insert(primaryBoard);
-    
+
+    MinPQ<SearchNode> primaryPQ = new MinPQ<SearchNode>();
+    // See http://algs4.cs.princeton.edu/code/javadoc/edu/princeton/cs/algs4/MinPQ.html for MinPQ
+    primaryPQ.insert(new SearchNode(initial, 0, null));
+
     // ----- [] Prepare a secondary board in case initial board is unsolvable
 
-    Board secondaryBoard = initial.twin();  // either primaryBoard or secondaryBoard will lead to a solution
-    MinPQ<Board> secondaryPQ = new MinPQ<Board>(Board.BY_HAMMING);  // http://algs4.cs.princeton.edu/code/javadoc/edu/princeton/cs/algs4/MinPQ.html
-    secondaryPQ.insert(secondaryBoard);
-    
-    boolean complete = false;
-    
-    while (!complete) {
-      
-      Iterable<Board> primaryNeighbors = primaryBoard.neighbors();
-      
-      for (Board board : primaryNeighbors) {
-        // add to primaryPQ if not equal to previousNode
+    MinPQ<SearchNode> secondaryPQ = new MinPQ<SearchNode>();
+    secondaryPQ.insert(new SearchNode(initial.twin(), 0, null));
 
+    // ----- [] Begin A* search algorithm
+
+    for (int i = 0; i < MAX_ITERS; i++) {
+
+      // ----- [] Process primary
+
+      SearchNode primarySearchNode = primaryPQ.delMin();
+
+      if (primarySearchNode.board.isGoal()) {
+        solvable = true;
+        SearchNode searchNode = primarySearchNode;
+        while (searchNode != null) {
+          solutionStack.push(searchNode.board);
+          searchNode = searchNode.prevSearchNode;
+        }
+        break;
       }
       
-      Iterable<Board> secondaryNeighbors = secondaryBoard.neighbors();
+      processSearchNode(primaryPQ, primarySearchNode);
+
+      // ----- [] Process secondary
+
+      SearchNode secondarySearchNode = secondaryPQ.delMin();
+
+      if (secondarySearchNode.board.isGoal()) {
+        break; // initial board is not solvable
+      }
       
+      processSearchNode(secondaryPQ, secondarySearchNode);
+
     }
-    
-    
+
   }
 
   /**
    * Return <tt>true</tt> if the provided board is solvable.
    */
   public boolean isSolvable() {
-    return false;
+    return solvable;
   }
 
   /**
@@ -69,6 +94,9 @@ public class Solver {
    * unsolvable.
    */
   public int moves() {
+    if (isSolvable()) {
+      return numMoves;
+    }
     return UNSOLVABLE;
   }
 
@@ -76,9 +104,46 @@ public class Solver {
    * Return the sequence of boards that leads to a solution; or <tt>null</tt> if unsolvable.
    */
   public Iterable<Board> solution() {
+    if (isSolvable()) {
+      return solutionStack;
+    }
     return null;
   }
-  
+
+  private void processSearchNode(MinPQ<SearchNode> minPQ, SearchNode searchNode) {
+    Iterable<Board> neighbors = searchNode.board.neighbors();
+    for (Board board : neighbors) {
+      // add to minPQ if not equal to previousNode
+      if (searchNode.prevSearchNode != null && !searchNode.prevSearchNode.board.equals(board)) {
+        minPQ.insert(new SearchNode(board, numMoves, searchNode));
+      }
+    }
+  }
+
+  /**
+   * Search Node helper class given in assignment.
+   */
+  private class SearchNode implements Comparable<SearchNode> {
+    Board board; // current board
+    SearchNode prevSearchNode; // previous search Node
+    int priority;
+
+    public SearchNode(Board board, int numMoves, SearchNode prevSearchNode) {
+      this.board = board;
+      this.prevSearchNode = prevSearchNode;
+      if (PRIORITY_FUNCTION_CHOICE == PriorityFunction.HAMMING) {
+        priority = numMoves + board.hamming();
+      } else {
+        priority = numMoves + board.manhattan();
+      }
+    }
+
+    @Override
+    public int compareTo(SearchNode otherSearchNode) {
+      return Integer.compare(priority, otherSearchNode.priority);
+    }
+  }
+
   /**
    * Solve a slider puzzle (provided by assignment).
    */
