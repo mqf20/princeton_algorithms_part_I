bdf8bdede613ba2a638d1d72c4a7bd419a6bbd3e
mq <mingqingfoo@gmail.com>
2017-06-11 21:33:15 +0800

    Cleaned up imports

--- src/priorityqueues/kpuzzle/Board.java
+++ src/priorityqueues/kpuzzle/Board.java
@@ -4,7 +4,6 @@ import java.util.Comparator;
 import java.util.Random;
 
 import edu.princeton.cs.algs4.Stack;
-import sort.StudentComparatorExample;
 
 /**
  * Board from assignment of Week 4, Coursera Algorithms, Part I
