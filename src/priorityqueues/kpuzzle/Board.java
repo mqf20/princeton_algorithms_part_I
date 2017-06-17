a0a9696ea02a11f4810a3ebd11294afa45e6ede8
mq <mingqingfoo@gmail.com>
2017-06-17 16:39:23 +0800

    optimized for memory using 1d short[]

--- src/priorityqueues/kpuzzle/Board.java
+++ src/priorityqueues/kpuzzle/Board.java
@@ -10,7 +10,7 @@ import edu.princeton.cs.algs4.Stack;
  */
 public class Board {
 
-  private int[][] blocks; // represents an n-by-n array of blocks
+  private short[] blocks; // represents an n-by-n array of blocks
   private int dimension;
 
   /**
@@ -22,9 +22,11 @@ public class Board {
       throw new NullPointerException("board cannot be null");
     }
     dimension = blocks.length;
+    if (dimension > 181) {
+      throw new IllegalArgumentException("Dimension is too large");
+    }
 
-    this.blocks = new int[dimension][dimension];
-
+    this.blocks = new short[dimension * dimension];
     boolean[] indices = new boolean[dimension * dimension + 1];
     for (int i = 0; i < dimension; i++) {
       if (blocks[i] == null || blocks[i].length != dimension) {
@@ -35,7 +37,8 @@ public class Board {
           throw new IllegalArgumentException("Duplicate labels");
         }
         indices[blocks[i][j]] = true;
-        this.blocks[i][j] = blocks[i][j];
+        this.blocks[i * dimension + j] = (short) blocks[i][j];
+
       }
     }
   }
@@ -44,7 +47,7 @@ public class Board {
    * Return the board dimension.
    */
   public int dimension() {
-    return blocks.length;
+    return dimension;
   }
 
   /**
@@ -54,7 +57,7 @@ public class Board {
     int hammingValue = 0;
     for (int i = 1; i <= dimension; i++) {
       for (int j = 1; j <= dimension; j++) {
-        int label = blocks[i - 1][j - 1];
+        int label = blocks[(i - 1) * dimension + (j - 1)];
         if (label != 0 && label != j + dimension * (i - 1)) { // ignore 0 label
           hammingValue++;
         }
@@ -70,7 +73,7 @@ public class Board {
     int manhattanValue = 0;
     for (int i = 1; i <= dimension; i++) {
       for (int j = 1; j <= dimension; j++) {
-        int label = blocks[i - 1][j - 1];
+        int label = blocks[(i - 1) * dimension + (j - 1)];
         if (label != 0 && label != j + dimension * (i - 1)) { // ignore 0 label
           int verticalOffset = Math.abs((label - 1) / dimension + 1 - i);
           int col = (label % dimension) == 0 ? dimension : (label % dimension);
@@ -88,7 +91,7 @@ public class Board {
   public boolean isGoal() {
     for (int i = 1; i <= dimension; i++) {
       for (int j = 1; j <= dimension; j++) {
-        int label = blocks[i - 1][j - 1];
+        int label = blocks[(i - 1) * dimension + (j - 1)];
         if (label != 0 && label != j + dimension * (i - 1)) { // ignore 0 label
           return false;
         }
@@ -110,8 +113,9 @@ public class Board {
     // keep trying to generate new random coordinates if:
     // - the two pairs of coordinates are equal, or
     // - either pair of coordinates point to an empty block (0)
-    while ((iRandom1 == iRandom2 && jRandom1 == jRandom2) || blocks[iRandom1][jRandom1] == 0
-        || blocks[iRandom2][jRandom2] == 0) {
+    while ((iRandom1 == iRandom2 && jRandom1 == jRandom2)
+        || blocks[iRandom1 * dimension + jRandom1] == 0
+        || blocks[iRandom2 * dimension + jRandom2] == 0) {
       iRandom1 = new Random().nextInt(dimension);
       jRandom1 = new Random().nextInt(dimension);
       iRandom2 = new Random().nextInt(dimension);
@@ -154,7 +158,7 @@ public class Board {
     while (i < dimension) {
       j = 0;
       while (j < dimension) {
-        if (blocks[i][j] == 0) {
+        if (blocks[i * dimension + j] == 0) {
           found = true;
           break;
         }
@@ -208,7 +212,7 @@ public class Board {
     stringBuilder.append(dimension + "\n");
     for (int i = 0; i < dimension; i++) {
       for (int j = 0; j < dimension; j++) {
-        stringBuilder.append(String.format(format, blocks[i][j]));
+        stringBuilder.append(String.format(format, blocks[i * dimension + j]));
       }
       stringBuilder.append("\n");
     }
@@ -222,7 +226,7 @@ public class Board {
     int[][] clone = new int[dimension][dimension];
     for (int i = 0; i < dimension; i++) {
       for (int j = 0; j < dimension; j++) {
-        clone[i][j] = blocks[i][j];
+        clone[i][j] = blocks[i * dimension + j];
       }
     }
     return clone;
